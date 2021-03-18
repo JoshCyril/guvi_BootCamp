b900feae75ba0ae162b406e6c72fa19ea63809dd
JPC8 <joshua.cyril88@gmail.com>
2021-03-18 23:04:50 +0530

    #3 completed

---
+++ Tasks/Week1/function_API-task/weatherAPI/script.js
@@ -0,0 +1,31 @@
+function weatherBalloon( latt, longn ) {
+    var key = 'adcc23c3b9f80e45999b1aa86a0f04a3';
+
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+ latt +'&lon='+ longn +'&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      console.log(data.weather[0].main);
+      console.log(data);
+      alert("City:  "+data.name+" |  Weather: "+data.weather[0].main);
+    })
+    .catch(function() {
+      // catch any errors
+      alert('Location is Blocked');
+    });
+  }
+  
+  window.onload = function() {    
+      navigator.geolocation.getCurrentPosition(setPosition, showError);
+    
+    }
+
+    function setPosition(position){
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        
+        weatherBalloon(latitude, longitude);
+    }
+
+    function showError(error){
+        alert('Location is Blocked');
+    }
